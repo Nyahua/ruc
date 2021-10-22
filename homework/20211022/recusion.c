d15f7aace9ef74c31cf27172dbb9e7d97b4f8897
root <root@DESKTOP-9FK7E09.localdomain>
2021-10-22 21:40:17 +0800

    digui

---
+++ homework/20211022/recusion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int A, B;
+
+int f(int n) {
+    if (n == 1) {
+        return 1;
+    }
+
+    if (n == 2) {
+        return 1;
+    }
+
+    return (A * f(n-1) + B * f(n-2)) % 7;
+}
+
+int main() {
+    int n;
+    scanf("%d %d %d", &A, &B, &n);
+    printf("%d\n", f(n));
+
+    return 0;
+}
