363e1169cb72e60afe009cfd5c665609a53b534d
root <root@DESKTOP-9FK7E09.localdomain>
2021-10-29 21:51:31 +0800

    matrix

--- homework/20211029/main.c
+++ homework/20211029/main.c
@@ -3,19 +3,16 @@
 int main() {
 	int N, P, M;
 	int i, j, k, mul; //muti是乘起来的两个数最后要把P个mul加在一起成为某一位的sum
-	int sum = 0;
+	// int sum = 0; 不需要了
 	scanf("%d %d %d", &N, &P, &M);
-
-    
-	int A[N][P], B[P][M], C[N][M];
-    // 这个数组的定义有问题：
-    // 在C语言中，不能这样定义没有确定范围的数组
-    // 在NPM不确定的情况下，会出现内存溢出
-    // 可以按照最大范围定义数组，
-    // 动态数组的定义可能在指针课程中讲
-    // int A[2][5], B[5][5], C[2][5];
-
 	getchar();
+    
+    // Variable-length arrays with automatic storage duration are allowed since C99. 
+    // In C89, it is not possible to allocate an array with automatic storage duration, 
+    // size of which is not known at the compile time.
+    // https://stackoverflow.com/a/14936379
+	int A[N][P], B[P][M], C[N][M];
+
 	for (i = 0; i < N; i ++) {
 		for (j = 0; j < P; j ++) {
 			scanf("%d", &A[i][j]);
@@ -28,24 +25,24 @@ int main() {
 			getchar();
 		}
 	}
-	
+
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < M; j++) {
+            C[i][j] = 0; // 矩阵元素初始化为0；
+            // 元素开始累加
 			for (k = 0; k < P; k ++) {
 				mul = A[i][k] * B[k][j];
-				sum = sum + mul;
-				C[i][j] = sum;
+				C[i][j] += mul;
 			}
-
-		}
-	}
-
-	for (i = 0; i < N; i ++) {
-		for (j = 0; j < M - 1; j ++) {
-			printf("%d", C[i][j]);
-		}
-		if (j == M) {
-			printf("%d\n", C[i][M]);
+            // 累加完毕，开始打印
+            printf("%d", C[i][j]);
+            // 最后一位回车，否则空格
+            if (j == (M - 1)) {
+                printf("\n");
+            }
+            else {
+                printf(" ");
+            }
 		}
 	}
 	return 0;
